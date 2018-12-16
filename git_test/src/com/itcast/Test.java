dbdbe5417aca474335ed4c058a73e2504e04c854
Likai <nono_7380>
2018-12-16 21:02:14 +0800

    使用Idea将修改Java工程添加到本地git

--- git_test/src/com/itcast/Test.java
+++ git_test/src/com/itcast/Test.java
@@ -4,4 +4,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello,world!");
     }
+
+    public void doTest(){
+        int i = 10;
+        i += 5;
+    }
 }
